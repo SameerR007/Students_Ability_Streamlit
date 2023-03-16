39dc897abfebf916c4aa570b74023d55a457b677
SameerR007 <100122935+SameerR007@users.noreply.github.com>
2023-03-16 18:01:14 +0530

    Update Students_Ability_flask.py

--- Students_Ability_flask.py
+++ Students_Ability_flask.py
@@ -1,14 +1,28 @@
 import streamlit as st
 def main():
-    import pandas as pd
-    import pickle
-    kmeans_new=pickle.load(open("model.pkl","rb"))
+   import pandas as pd
+    import seaborn as sns
+    from sklearn.cluster import KMeans
+    from sklearn import preprocessing
+    from matplotlib import pyplot as plt
+    data=pd.read_excel("DUET Data - Copy.xlsx")
+    data=data.dropna()
+    data=data[data['Graduation Marks']>=50]
+    x=data[['Graduation Marks','Entrance Marks']]
+    x_scaled=preprocessing.scale(x)
+    kmeans_new=KMeans(4,random_state=1)
+    kmeans_new.fit(x_scaled)
+    clusters_new=x.copy()
+    clusters_new['clusters']=kmeans_new.fit_predict(x_scaled)
+    
+    
     clusters_new=pd.read_excel("clusters_new.xlsx")
+    
     gm=st.number_input("Graduation Marks")
     em=st.number_input("Entrance Marks")
     input_para=[gm,em]
-    input_para[0]=(input_para[0]-77.3172)/9.2327
-    input_para[1]=(input_para[1]-127.9162)/27.4483
+    input_para[0]=(input_para[0]-data['Graduation Marks'].mean())/data['Graduation Marks'].std()
+    input_para[1]=(input_para[1]-data['Entrance Marks'].mean())/data['Entrance Marks'].std()
     if st.button("Result"):
 
         if (kmeans_new.predict([input_para])==0):
